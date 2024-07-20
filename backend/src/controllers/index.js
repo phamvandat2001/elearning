117eab8d52ff7c46e0ef587fd6e7d4e7e58e2764
Dat Pham Van <ph.dat.2k1@gmail.com>
2024-07-20 23:08:12 +0700

    refactor: remove backend, frontend

--- backend/src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-  const controllers = app.fs.readdirSync('src/controllers').filter((name) => name.endsWith('.controller.js'));
-  for (const controllerName of controllers) {
-    require(app.path.join(__dirname, controllerName))(app);
-  }
-  console.log('- Load all controllers successful!');
-};
--- backend/src/lib/fs.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const { promises, ...fse } = require("fs-extra");
-  const path = require("path");
-  app.fs = {
-    ...promises,
-    ...fse
-  };
-  app.path = { ...path };
-};
