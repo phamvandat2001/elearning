ae5938cd8e86e257b9468aa15deed79b8c5a4c7c
Dat Pham Van <ph.dat.2k1@gmail.com>
2024-10-04 00:18:06 +0700

    purge: js backend

--- frontend/src/core/layout/MainLayout/index.tsx
+++ frontend/src/core/layout/MainLayout/index.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Layout from 'antd/es/layout/index';
 import Menu from "antd/es/menu/index";
+import type { ItemType, MenuItemType } from "antd/es/menu/interface";
 
 import classes from "./index.module.scss";
 
@@ -13,14 +14,34 @@ interface MainLayoutProps {
 const { Content, Header, Sider } = Layout;
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const items: ItemType<MenuItemType>[] = [
+    {
+      icon: <i className="fa fa-regular fa-chart-bar" />,
+      label: 'Dashboard',
+      key: 'dashboard',
+      onClick: () => {}
+    },
+    {
+      icon: <i className="fa fa-regular fa-circle-user" />,
+      label: 'User',
+      key: 'user',
+    },
+    {
+      icon: <i className="fa fa-regular fa-circle-user" />,
+      label: 'Menu with submenu',
+      key: 'menu-with-submenu',
+      children: [1,2,3,4,5].map((i) => ({ key: i, label: `Option ${i}` })),
+    }
+  ];
   return (
     <Layout style={{ height: "100%" }}>
-      <Sider width="15%" collapsible className={classes.sider}>
-        <Menu />
+      <Sider width="15%" className={classes.sider} color="gray">
+        <div className={classes.logo}></div>
+        <Menu items={items} mode="inline" />
       </Sider>
       <Layout>
         <Header className={classes.header}>
-          Top header
+          
         </Header>
         <Content className={classes.content}>
           {children}
